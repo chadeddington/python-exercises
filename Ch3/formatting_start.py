48ec85ff57c7b51a92937b0499022d75c37e0f61
Chad Eddington <edd11001@byui.edu>
2016-01-08 17:33:38 -0700

    Stuff with dates

--- Ch3/formatting_start.py
+++ Ch3/formatting_start.py
@@ -1,10 +1,9 @@
-#
-# Example file for formatting time and date output
-# (For Python 3.x, be sure to use the ExampleSnippets3.txt file)
-
 from datetime import datetime
 
 def main():
+	now = datetime.now()
+	# datetime control string
+	print now.strftime("%x")
 
 if __name__ == "__main__":
   main();
--- Ch3/timedeltas_start.py
+++ Ch3/timedeltas_start.py
@@ -5,3 +5,22 @@
 from datetime import date
 from datetime import time
 from datetime import datetime
+from datetime import timedelta
+
+ayear = timedelta(days=365)
+now = datetime.now()
+
+# Todays date a year from now
+# print "One year from now it will be " + str(now + ayear);
+
+# One week ago
+t = now - timedelta(weeks=1)
+s = t.strftime("%A %B %d, %Y")
+# print "A week from now it was " + s
+
+# Days until Christmas
+today = date.today()
+christmas = date(today.year, 12, 25)
+
+# String arguments for injecting a variable
+print "Christmas is %s days away" % (christmas - today).days 
